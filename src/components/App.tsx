e1a4add108fd11e9e2240c3a6f7cd8403fec443a
tayutaedomo <tayutaedomo@gmail.com>
2021-05-09 15:54:28 +0900

    add: firebase auth handling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,14 @@
-import React, { VFC } from 'react';
+import React, { useEffect, VFC } from 'react';
+
+import { signInAnonymously } from '../utils/firebase';
 
 import Router from './router/Router';
 
 const App: VFC = () => {
+  useEffect(() => {
+    signInAnonymously();
+  }, []);
+
   return (
     <>
       <Router />
